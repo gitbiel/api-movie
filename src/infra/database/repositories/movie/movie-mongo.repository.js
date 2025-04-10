58c95be45c1f6e3ae7d4b94f0756d794ccb09779
Gabriel Mendes <gabrielzmendes1@gmail.com>
2025-04-10 13:46:13 -0300

    feat: remove MongoDB metadata from movie responses

--- src/infra/database/repositories/movie/movie-mongo.repository.js
+++ src/infra/database/repositories/movie/movie-mongo.repository.js
@@ -1,24 +1,33 @@
 import { MovieModel } from '../../mongoose/schemas/movie.schema.js';
 
+function cleanMovieData(movie) {
+  const { _id, __v, createdAt, updatedAt, ...rest } = movie;
+  return rest;
+}
+
 export class MovieMongoRepository {
   async create(movieData) {
     const movie = new MovieModel(movieData);
     await movie.save();
-    return movie.toObject();
+    return cleanMovieData(movie.toObject());
   }
 
   async findAll() {
-    return MovieModel.find().lean();
+    const movies = await MovieModel.find().lean();
+    return movies.map(cleanMovieData);
   }
 
   async findById(id) {
-    return MovieModel.findOne({ id });
+    const movie = await MovieModel.findOne({ id }).lean();
+    return movie ? cleanMovieData(movie) : null;
   }
 
   async update(id, data) {
-    return await MovieModel.findOneAndUpdate({ id }, data, {
+    const movie = await MovieModel.findOneAndUpdate({ id }, data, {
       new: true,
+      lean: true,
     });
+    return movie ? cleanMovieData(movie) : null;
   }
 
   async delete(id) {
